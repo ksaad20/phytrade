ec6d22294b5ad9a146168551f7ff4e8cc3bf69fc
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-04-03 07:50:52 +0600

    Update __init__.py

--- src/phytrade/__init__.py
+++ src/phytrade/__init__.py
@@ -1,4 +1,22 @@
-from .thermo.engine import ThermoArbitrator
+"""
+phytrade: Institutional Physics Library for Global Commodity Arbitration
+Version: 1.2.5
+Author: Kazi Saad Asif
+"""
 
-__version__ = "1.2.3"
+from .thermo import ThermoArbitrator
+from .fluids import FluidsArbitrator
+from .mechanical import MechanicalArbitrator
+from .logistics import LogisticsArbitrator
+from .strategic import StrategicArbitrator
+
+__version__ = "1.2.5"
 __author__ = "Kazi Saad Asif"
+
+__all__ = [
+    "ThermoArbitrator",
+    "FluidsArbitrator",
+    "MechanicalArbitrator",
+    "LogisticsArbitrator",
+    "StrategicArbitrator",
+]
